a83d44acccf02054e8b7c62aaf56e6ce6a90102e
SarahSeel <sarahizsilly09@yahoo.com>
2022-08-25 22:45:22 -0500

    HomeWorkWeek2.js

---
+++ Week2/HomeWorkWeek2.js
@@ -0,0 +1,15 @@
+// First portion of HW
+
+let pizzaPlace = "Imo's Pizza"; // String
+let numberOfToppings = 2; // Num
+
+// Second Portion of HW
+`"${pizzaPlace} has many great options of pizza toppings, but I only like ${numberOfToppings}. Which is Hamburger and Bacon."`;
+
+// Third Portion of HW
+
+if (numberOfToppings > 10) {
+  console.log("A whole lot of pizza.");
+} else {
+  console.log("Quality, not quantity.");
+}
